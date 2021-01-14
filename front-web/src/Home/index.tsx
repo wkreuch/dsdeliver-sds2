a3fc115baaf346e824785d83061157a400eae6bc
william kreuch <williamkreuch@gmail.com>
2021-01-14 00:32:36 -0300

    Add react router + Add component StepsHeader

--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,6 +1,7 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
 import Footer from '../Footer';
+import { Link } from 'react-router-dom';
 function Home() {
     return (
         <>
@@ -9,7 +10,7 @@ function Home() {
                     <div className="home-action">
                         <h1 className="home-title"> Faça seu pedido <br /> que entregamos <br /> para você!!!</h1>
                         <h3 className="hpme-subtitle"> Escolha seu pedido e em poucos minutos <br /> levaremos a sua porta</h3>
-                        <a href="pedido" className="home-btn-order">FAZER PEDIDO</a>
+                        <Link to="orders" className="home-btn-order">FAZER PEDIDO</Link>
                     </div>
                     <div className="home-image">
                         <MainImage />
---
+++ front-web/src/Orders/index.tsx
@@ -0,0 +1,13 @@
+import StepHeader from './StepHeader';
+import './styles.css';
+
+function Orders() {
+    return (
+        <div className="orders-container">
+            <StepHeader />
+            
+        </div>
+    )
+}
+
+export default Orders;
