b0a9904a84f7b368c12fb3175b44ec7c1633418d
william kreuch <williamkreuch@gmail.com>
2021-01-13 23:50:16 -0300

    Add component Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,11 @@
+import './styles.css';
+
+function Footer() {
+    return (
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª edição do evento Semana DevSuperior
+        </footer>
+    )
+}
+
+export default Footer;
