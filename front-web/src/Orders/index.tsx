443ca07dcb7da37bfd86be6b8113787e2025f9c6
william kreuch <williamkreuch@gmail.com>
2021-01-16 16:37:53 -0300

    Add component Orders + axios

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from 'react';
+import { fetchProducts } from '../api';
+import ProducstList from './ProducstList';
 import StepHeader from './StepHeader';
 import './styles.css';
+import { Product } from './types';
 
 function Orders() {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        fetchProducts()
+            .then(response => setProducts(response.data))
+            .catch(error => console.error(error));
+    }, []);
+
     return (
         <div className="orders-container">
             <StepHeader />
-            
+            <ProducstList products={products} />
         </div>
     )
 }
