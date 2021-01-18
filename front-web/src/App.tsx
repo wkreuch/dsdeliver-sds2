67652e3e3ac6c3bc4832b22953f25fbc23b076a4
william kreuch <williamkreuch@gmail.com>
2021-01-17 23:03:51 -0300

    Add component OrderSummary + fazer pedido + react-toastify

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,15 @@
 import './App.css';
 import Routes from './Routes';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <div>
+      <>
         <Routes />
+        <ToastContainer />
+      </>
     </div>
   );
 }
--- front-web/src/Orders/OrderLocation.tsx
+++ front-web/src/Orders/OrderLocation.tsx
@@ -2,12 +2,12 @@ import { useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import AsyncSelect from 'react-select/async';
 import { fetchLocalMapBox } from '../api';
-import { OrderLocationdata } from './types';
+import { OrderLocationData } from './types';
 
 const initialPosition = {
-    lat: 51.505,
-    lng: -0.09
-}
+    lat: -23.5679266,
+    lng: -46.6423213
+};
 
 type Place = {
     label?: string,
@@ -19,7 +19,7 @@ type Place = {
 };
 
 type Props = {
-    onChangeLocation: (location: OrderLocationdata) => void;
+    onChangeLocation: (location: OrderLocationData) => void;
 }
 
 function OrderLocation({ onChangeLocation }: Props) {
